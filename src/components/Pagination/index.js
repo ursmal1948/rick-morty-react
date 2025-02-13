25f4bc41612bf26e449b37b7d47bd1bc6394e3b3
ursmal1948 <ula.malin35@gmail.com>
2025-02-13 18:57:46 +0100

    Pagination arrows added

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
-import { Wrapper, Button, PageNumber ,  PageIndicator,TextButton} from "./styled";
+import { Wrapper, Button, PageNumber, PageIndicator, ButtonText } from "./styled";
+import { BackwardVector, ForwardVector } from "./arrows";
+import { useState } from "react";
 
 const useQueryParameter = (key) => {
     const location = useLocation();
@@ -30,35 +32,66 @@ const useReplaceQueryParameter = () => {
 
 const Pagination = ({ page, totalPages }) => {
 
+    const [width, setWidth] = useState(window.innerWidth);
+    window.onresize = () => setWidth(window.innerWidth);
+
     const replaceQParameter = useReplaceQueryParameter();
     const currentPage = parseInt(page);
     const firstPage = 1;
     const previousPage = currentPage - 1;
     const nextPage = currentPage + 1;
-    const pages = totalPages > 20 ? 20 : totalPages;
+    const pages = totalPages
     const lastPage = pages;
 
+    const isScreenSmall = width < 767;
 
     const goToPage = (targetPage) => {
         replaceQParameter({
-            key:"page",
+            key: "page",
             value: targetPage,
         });
+
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
     };
 
     return (
         <Wrapper>
             <Button onClick={() => goToPage(firstPage)}
-                disabled={currentPage===1?true:false}
-                >
+                disabled={currentPage === 1 ? true : false}
+            >
+                <BackwardVector
 
-                First
+                    disabled={currentPage == 1 ? true : false}
+                />
+                {isScreenSmall && (
+                    <BackwardVector
+
+                        disabled={
+
+                            currentPage == 1 ? true : false}
+                    />
+                )}
+                <ButtonText>
+                    First
+                </ButtonText>
+
             </Button>
 
             <Button onClick={() => goToPage(previousPage)}
-                disabled={currentPage===1?true:false}
-                >
-                Previous
+                disabled={currentPage === 1 ? true : false}
+            >
+                <BackwardVector
+                    disabled={currentPage == 1 ? true : false}
+
+                />
+                <ButtonText>
+                    Previous
+                </ButtonText>
+
+
             </Button>
             <PageIndicator>
                 <PageNumber>{currentPage} </PageNumber> of{" "}
@@ -67,19 +100,32 @@ const Pagination = ({ page, totalPages }) => {
             </PageIndicator>
 
             <Button onClick={() => goToPage(nextPage)}
-                disabled={currentPage===20?true:false}
-                >
-                <TextButton>
+                disabled={currentPage === pages ? true : false}
+            >
+                <ButtonText>
                     Next
-                </TextButton>
+                </ButtonText>
+                <ForwardVector
+                    disabled={currentPage == pages ? true : false}
+                />
 
             </Button>
 
             <Button onClick={() => goToPage(lastPage)}
-                disabled={currentPage===20?true:false}
-                >
+                disabled={currentPage === pages ? true : false}
+            >
 
-                Last
+                <ButtonText>
+                    Last
+                </ButtonText>
+                <ForwardVector
+                    disabled={currentPage == pages ? true : false}
+                />
+                {isScreenSmall && (
+                    <ForwardVector
+                        disabled={currentPage == pages ? true : false}
+                    />
+                )}
             </Button>
 
 
