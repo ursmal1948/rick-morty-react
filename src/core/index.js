e6576458545db45beca741d9ae9526946ab20c53
ursmal1948 <ula.malin35@gmail.com>
2025-02-15 15:19:17 +0100

    ROuting setup with navigation logic

--- src/core/index.js
+++ src/core/index.js
@@ -1,35 +1,25 @@
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import { CharactersList } from "../components/CharactersList";
-import {Header,Card,Image} from 'semantic-ui-react'
+import CharactersList from "../features/CharactersList";
+import CharacterDetails from "../features/CharacterDetails";
+import PageHeader from "../components/PageHeader";
 
-
-function App(){
-    return (
-        <>
-        <HashRouter>
-
-
-
-             <Routes>
-            {/* <Route path="/character/:id">
-
-            // </Route> */} 
-
-
-            <Route path="/characters" element={<CharactersList/>}/>
-
-
-            <Route path="/" element={<Navigate to="/characters?page=1" replace />} />
-        
-
-
-
-
-            </Routes>
-        </HashRouter>
-            
-        </>
-    )
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <PageHeader />
+        <Routes>
+          <Route path="/characters/:id" element={<CharacterDetails />} />
+          <Route path="/characters" element={<CharactersList />} />
+          <Route
+            path="/"
+            element={<Navigate to="/characters?page=1" replace />}
+          />
+          {/* <Route path="/loader" element={<Loader />} /> */}
+        </Routes>
+      </HashRouter>
+    </>
+  );
 }
 export default App;
