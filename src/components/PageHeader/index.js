59342ade72a3459d019c6e7e778e4fa76991e958
ursmal1948 <ula.malin35@gmail.com>
2025-02-15 15:10:32 +0100

    added page header component

---
+++ src/components/PageHeader/index.js
@@ -0,0 +1,11 @@
+import { Wrapper } from "./styled";
+import { StyledHeader } from "../StyledHeader/styled";
+const PageHeader = () => {
+  return (
+    <Wrapper>
+      <StyledHeader pageHeaderFlag>RICK & MORTY</StyledHeader>
+    </Wrapper>
+  );
+};
+
+export default PageHeader;
---
+++ src/components/PageHeader/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(135deg, #00bfff, #ffd700);
+  height: 92px;
+`;
