5948de152cb19837823e27af97765ca6432cec1b
ursmal1948 <ula.malin35@gmail.com>
2025-02-17 01:39:31 +0100

    Change in the comment of error in getCharacters function

--- src/features/api/apiData.js
+++ src/features/api/apiData.js
@@ -1,43 +1,45 @@
-import axios from "axios"
+import axios from "axios";
 
-const baseUrl = 'https://rickandmortyapi.com/api';
+const baseUrl = "https://rickandmortyapi.com/api";
 
-export const getCharacters = async (page ) => {
-    try {
-        const response = await axios.get(`${baseUrl}/character?page=${page}`)
+export const getCharacters = async (page) => {
+  try {
+    const response = await axios.get(`${baseUrl}/character?page=${page}`);
 
-        if (!response || !response.data) {
-            throw new Error(`Invalid getMovies response data for page: ${page}`);
-        }
-        return response.data;
-    } catch (error) {
-        console.error(error)
+    if (!response || !response.data) {
+      throw new Error(`Invalid getMovies response data for page`);
     }
-}
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getCharacterDetails = async (characterId) => {
-    try {
-        const response = await axios.get(`${baseUrl}/character/${characterId}`);
+  try {
+    const response = await axios.get(`${baseUrl}/character/${characterId}`);
 
-        if (!response || !response.data) {
-            throw new Error(`Invalid response for getting character's details with id: ${characterId}`);
-        }
-        return response.data;
-    } catch (error) {
-        console.error(error)
+    if (!response || !response.data) {
+      throw new Error(
+        `Invalid response for getting character's details with id: ${characterId}`
+      );
     }
-}
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const getCharactersByStatus = async (status='alive',page=1) => {
-    try{
-        const response = await axios.get(`${baseUrl}/character?status=${status}&page=${page}`);
-        if (!response || !response.data) {
-            throw new Error(`Invalid response for filtering by status: ${status}`);
-        }
-        return response.data;
-        
-
-    }catch(error){
-        console.log(error);
+export const getCharactersByStatus = async (status = "alive", page = 1) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/character?status=${status}&page=${page}`
+    );
+    if (!response || !response.data) {
+      throw new Error(`Invalid response for filtering by status: ${status}`);
     }
-}
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
